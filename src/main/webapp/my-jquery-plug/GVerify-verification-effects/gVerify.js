52f5aca53252166d3bae04a615a2f07be80c84bf
weifuzi <958252871@qq.com>
2017-09-15 01:00:10 +0800

    login提交

--- src/main/webapp/my-jquery-plug/GVerify-verification-effects/gVerify.js
+++ src/main/webapp/my-jquery-plug/GVerify-verification-effects/gVerify.js
@@ -3,8 +3,8 @@
 		this.options = { //默认options参数值
 			id: "", //容器Id
 			canvasId: "verifyCanvas", //canvas的ID
-			width: "100", //默认canvas宽度
-			height: "30", //默认canvas高度
+			width: "40", //默认canvas宽度
+			height: "10", //默认canvas高度
 			type: "blend", //图形验证码默认类型blend:数字字母混合类型、number:纯数字、letter:纯字母
 			code: ""
 		}
@@ -32,8 +32,8 @@
 		_init: function() {
 			var con = document.getElementById(this.options.id);
 			var canvas = document.createElement("canvas");
-			this.options.width = con.offsetWidth > 0 ? con.offsetWidth : "100";
-			this.options.height = con.offsetHeight > 0 ? con.offsetHeight : "30";
+			this.options.width = 150;//con.offsetWidth > 0 ? con.offsetWidth : "40";
+			this.options.height = 45;//con.offsetHeight > 0 ? con.offsetHeight : "10";
 			canvas.id = this.options.canvasId;
 			canvas.width = this.options.width;
 			canvas.height = this.options.height;
